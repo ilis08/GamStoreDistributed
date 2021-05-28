fc0831b6b091fe5c037757140f3a8a307b162c26
Iliya Taudd <iliya2002pl.ru@gmail.com>
2021-05-28 10:43:05 +0300

    Add WebAPI + MVC.

--- ApplicationService/DTOs/GameDTO.cs
+++ ApplicationService/DTOs/GameDTO.cs
@@ -9,9 +9,7 @@ namespace ApplicationService.DTOs
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
         public DateTime? Release { get; set; }
-        public string Image { get; set; }
         public double Price { get; set; }
-        public bool Available { get; set; }
 
         public int CategoryId { get; set; }
         public virtual CategoryDTO Category { get; set; }
---
+++ Data/Migrations/202105280731389_InitialCreate.cs
@@ -0,0 +1,54 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        CreatedBy = c.Int(nullable: false),
+                        CreatedOn = c.DateTime(),
+                        UpdatedBy = c.Int(nullable: false),
+                        UpdatedOn = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Games",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        ShortDescription = c.String(),
+                        LongDescription = c.String(),
+                        Release = c.DateTime(),
+                        Price = c.Double(nullable: false),
+                        CategoryId = c.Int(nullable: false),
+                        CreatedBy = c.Int(nullable: false),
+                        CreatedOn = c.DateTime(),
+                        UpdatedBy = c.Int(nullable: false),
+                        UpdatedOn = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Games", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Games", new[] { "CategoryId" });
+            DropTable("dbo.Games");
+            DropTable("dbo.Categories");
+        }
+    }
+}
